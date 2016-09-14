4cf7f490259f71bfa67031d4b17563cac58d7622
zujko <zujko@csh.rit.edu>
2016-09-14 12:54:21 -0400

    Create secrets template

---
+++ README.md
@@ -0,0 +1,2 @@
+# PawPrints [![Build Status](https://travis-ci.org/ritstudentgovernment/PawPrints.svg?branch=master)](https://travis-ci.org/ritstudentgovernment/PawPrints)
+PawPrints petition application for the RIT community.
--- pawprints/settings.py
+++ pawprints/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+from pawprints import secrets 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ('PAWPRINTS_SECRET_KEY')
+SECRET_KEY = secrets.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
