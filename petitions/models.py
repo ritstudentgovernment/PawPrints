ec04e34036337c97f4ce214f066ba3276ffb9335
zujko <zujko@csh.rit.edu>
2016-09-19 15:39:22 -0400

    Add response model

--- petitions/models.py
+++ petitions/models.py
@@ -10,14 +10,15 @@ class Petition(models.Model):
     tags = models.ManyToManyField(Tag) 
     author = models.ForeignKey(User)
     signatures = models.PositiveIntegerField()
-    created = models.DateTimeField()
+    created_at = models.DateTimeField()
     published = models.BooleanField(default=False)
     expires = models.DateTimeField()
     last_signed = models.DateTimeField()
+    has_response = models.BooleanField(default=False)
+    response = models.ForeignKey(Response, default=None, blank=True, null=True)
 
     def __unicode__(self):
         return self.title
-
 
 #
 # Defines tag model.
@@ -27,3 +28,14 @@ class Tag(models.Model):
 
     def __unicode__(self):
         return self.name
+
+#
+# Defines a response model.
+#
+class Response(models.Model):
+    description = models.TextField()
+    created_at = models.DateTimeField()
+    author = models.TextField()
+
+    def __unicode__(self):
+        return self.author
