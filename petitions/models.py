82edf432f857f16ec60cc62657e087be0e2a4f05
zujko <zujko@csh.rit.edu>
2016-09-19 15:09:24 -0400

    Added comments and published, expires, and last_signed field

--- petitions/models.py
+++ petitions/models.py
@@ -1,18 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
+#
+# Defines petition model.
+#
 class Petition(models.Model):
     title = models.CharField(max_length=80)
     description = models.TextField()
     tags = models.ManyToManyField(Tag) 
     author = models.ForeignKey(User)
     signatures = models.PositiveIntegerField()
+    created = models.DateTimeField()
+    published = models.BooleanField(default=False)
+    expires = models.DateTimeField()
+    last_signed = models.DateTimeField()
 
     def __unicode__(self):
         return self.title
 
 
+#
+# Defines tag model.
+#
 class Tag(models.Model):
     name = models.CharField(max_length=50)
 
