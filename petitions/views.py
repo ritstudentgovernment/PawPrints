cfada214b91307a5f546dfd33fa38f63c461af7a
zujko <zujko@csh.rit.edu>
2016-09-26 15:22:55 -0400

    Created initial petition view

--- petitions/views.py
+++ petitions/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, render
+from petitions.models import Petition
+from profile.models import Profile
 
-# Create your views here.
+def petition(request, petition_id):
+    petition = get_object_or_404(Petition, pk=petition_id) 
+    author = Profile.objects.get(petitions_created=petition)
+    user = request.user
+    users_signed = Profile.objects.filter(petitions_signed=petition)
+
+    data_object = {
+        'petition': petition,
+        'current_user': user,
+        'users_signed': users_signed
+    }
+
+    return render(request, '', data_object)      
