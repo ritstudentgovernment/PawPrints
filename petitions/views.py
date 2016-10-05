3e044c4adcd1ac13f18d6383e5cf1ec0fca48488
zujko <zujko@csh.rit.edu>
2016-10-05 15:37:33 -0400

    Added comments to view functions

--- petitions/views.py
+++ petitions/views.py
@@ -8,13 +8,21 @@ from profile.models import Profile
 from django.contrib.auth.models import User
 
 def petition(request, petition_id):
+    """ Handles displaying A single petition. 
+    DB queried to get Petition object and User objects.
+    User object queries retrieve author
+    and list of all users who signed the petition.
+    """
+    # Get petition of given id, if not found, display 404
     petition = get_object_or_404(Petition, pk=petition_id) 
+    # Get author of the petition
     author = User.objects.get(pk=petition.author.id)
     user = request.user
 
     # Check if user is authenticated before querying
     curr_user_signed = user.profile.partner_set.filter(petitions_signed=petition).exists() if user.is_authenticated() else None
 
+    # Get QuerySet of all users who signed this petition
     users_signed = Profile.objects.filter(petitions_signed=petition)
     
     data_object = {
@@ -29,6 +37,10 @@ def petition(request, petition_id):
 @login_required
 @require_POST
 def petition_sign(request, petition_id):
+    """ Endpoint for signing a petition.
+    This endpoint requires the user be signed in
+    and that the HTTP request method is a POST.
+    """
     petition = get_object_or_404(Petition, pk=petition_id)
     user = request.user
     user.profile.petitions_signed.add(petition)
@@ -43,6 +55,11 @@ def petition_sign(request, petition_id):
 @require_POST
 @user_passes_test(lambda u: u.is_staff)
 def petition_unpublish(request, petition_id):
+    """ Endpoint for unpublishing a petition.
+    This endpoint requires that the user be signed in,
+    the HTTP request method is a POST, and that the 
+    user is an admin.
+    """
     petition = get_object_or_404(Petition, pk=petition_id)
     petition.published = False    
     petition.save()
