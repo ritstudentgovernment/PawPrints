3b0cbbb23b55be6ca2823088a2a6c6db09db61f4
zujko <peter@zujko.xyz>
2017-02-15 11:18:24 -0500

    Clean up send_email urls file

--- send_mail/urls.py
+++ send_mail/urls.py
@@ -2,12 +2,3 @@ from django.conf.urls import url
 from . import views
 
 app_name='send_mail'
-
-urlpatterns = [
-	url(r'^approved/(?P<petition_id>\d+)/(?P<recipients>[\w.%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4})/', views.petition_approved),
-	url(r'^rejected/(?P<petition_id>\d+)/(?P<recipients>[\w.%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4})/(?P<message>[\w ]+)/$', views.petition_rejected),
-	url(r'^updated/(?P<petition_id>\d+)/(?P<recipients>[\w.%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4})/', views.petition_update),
-	url(r'^reached/(?P<petition_id>\d+)/(?P<recipients>[\w.%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4})/', views.petition_reached),
-	url(r'^report/(?P<petition_id>\d+)/(?P<recipients>[\w.%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4})/(?P<reason>[\w\-]+)/$', views.petition_report),
-	url(r'^received/(?P<petition_id>\d+)/(?P<recipients>[\w.%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4})/', views.petition_received),
-]
