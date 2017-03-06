f9a38e7a6f8196a28e304e1d73523520c7662631
zujko <peter@zujko.xyz>
2017-03-06 10:26:42 -0500

    Initial Celery setup

---
+++ pawprints/celery.py
@@ -0,0 +1,19 @@
+"""
+Defines Celery settings.
+"""
+
+from __future__ import absolute_import, unicode_literals
+import os
+from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pawprints.settings')
+
+app = Celery('pawprints')
+
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+app.autodiscover_tasks()
+
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
