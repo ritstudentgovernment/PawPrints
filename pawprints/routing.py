d8002c718fde9299265be774862b438e2d6fa844
zujko <peter@zujko.xyz>
2017-03-07 14:33:51 -0500

    Remove unused import

--- pawprints/routing.py
+++ pawprints/routing.py
@@ -5,7 +5,6 @@ Desc: Implements channels routing for the whole pawprints app.
 """
 from channels.routing import route
 from channels import include
-from send_mail.routing import channel_routing
 
 channel_routing = [
     include("petitions.routing.channel_routing"),
