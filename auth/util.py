d6fa3872127b35d35c755522a0bad83dc53de7a7
zujko <peter@zujko.xyz>
2017-05-02 12:15:27 -0400

    Update request object for saml

--- auth/util.py
+++ auth/util.py
@@ -1,10 +1,10 @@
 def prepare_django_request(request):
     # If server is behind proxys or balancers use the HTTP_X_FORWARDED fields
     result = {
-        'https': 'on' if request.is_secure() else 'off',
+        'https': 'on' if request.META['HTTP_X_FORWARDED_PROTO'] == 'https' else 'off',
         'http_host': request.META['HTTP_HOST'],
         'script_name': request.META['PATH_INFO'],
-        'server_port': request.META['SERVER_PORT'],
+        'server_port': request.META['HTTP_X_FORWARDED_PORT'],
         'get_data': request.GET.copy(),
         'post_data': request.POST.copy()
     }
