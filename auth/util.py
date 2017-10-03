cfee9726caa18ed1bdee1b06027274bfabc786b4
Lukas Yelle <lxy5611@g.rit.edu>
2017-10-03 17:02:41 -0400

    Reverts back to working commit and fixes a bug.

--- auth/util.py
+++ auth/util.py
@@ -1,22 +1,11 @@
 def prepare_django_request(request):
     # If server is behind proxys or balancers use the HTTP_X_FORWARDED fields
-    http_host = request.META.get('HTTP_HOST', None)
-    if 'HTTP_X_FORWARDED_FOR' in request.META:
-        # This is only true for my particular installations.
-        server_port = None
-        https = request.META.get('HTTP_X_FORWARDED_PROTO') == 'https'
-    else:
-        server_port = request.META.get('SERVER_PORT')
-        https = request.is_secure()
-    prepared = {
-        'https': 'on' if https else 'off',
-        'http_host': http_host,
+    result = {
+        'https': 'on' if request.META['HTTP_X_FORWARDED_PROTO'] == 'https' else 'off',
+        'http_host': request.META['HTTP_HOST'],
         'script_name': request.META['PATH_INFO'],
+        'server_port': request.META['HTTP_X_FORWARDED_PORT'],
         'get_data': request.GET.copy(),
         'post_data': request.POST.copy()
     }
-    if server_port:
-        # Empty port will make a (lonely) colon ':' appear on the URL, so
-        # it's better not to include it at all.
-        prepared['server_port'] = server_port
-    return prepared
+    return result
