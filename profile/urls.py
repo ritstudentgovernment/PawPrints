09c5a0f6c9e2419ad4af4225fcdb35c8163becd9
Lukas Yelle <lxy5611@g.rit.edu>
2017-10-24 10:43:58 -0400

    Updates URLS for manage endpoint

--- profile/urls.py
+++ profile/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     url(r'^$', views.profile),
     url(r'^settings/notifications/(?P<user_id>\d+)$', views.update_notifications, name='notification_update'),
     url(r'^manage/staff',views.manage_staff),
-    url(r'^manage/staff/superuser/add/(?P<user_id>\d+)$',views.add_superuser),
-    url(r'^manage/staff/superuser/remove/(?P<user_id>\d+)$',views.remove_superuser),
-    url(r'^manage/staff/staff_member/add/(?P<user_id>\d+)$',views.add_staff_member),
-    url(r'^manage/staff/staff_member/remove/(?P<user_id>\d+)$',views.remove_staff_member)
+    url(r'^manage/superuser/add/(?P<user_id>\d+)$',views.add_superuser),
+    url(r'^manage/superuser/remove/(?P<user_id>\d+)$',views.remove_superuser),
+    url(r'^manage/staff_member/add/(?P<user_id>\d+)$',views.add_staff_member),
+    url(r'^manage/staff_member/remove/(?P<user_id>\d+)$',views.remove_staff_member)
 ]
