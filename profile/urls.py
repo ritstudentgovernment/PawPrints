4c9bf1444e1bfe975964c765a47cfcdaf34b4703
Lukas Yelle <lxy5611@g.rit.edu>
2017-10-24 11:05:51 -0400

    Debugs Staff manager

--- profile/urls.py
+++ profile/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     url(r'^$', views.profile),
     url(r'^settings/notifications/(?P<user_id>\d+)$', views.update_notifications, name='notification_update'),
     url(r'^manage/staff',views.manage_staff),
-    url(r'^manage/superuser/add/(?P<user_id>\d+)$',views.add_superuser),
-    url(r'^manage/superuser/remove/(?P<user_id>\d+)$',views.remove_superuser),
-    url(r'^manage/staffmember/add/(?P<user_id>\d+)$',views.add_staff_member),
-    url(r'^manage/staffmember/remove/(?P<user_id>\d+)$',views.remove_staff_member)
+    url(r'^manage/admin/add/(?P<user_id>\d+)$',views.add_superuser),
+    url(r'^manage/admin/remove/(?P<user_id>\d+)$',views.remove_superuser),
+    url(r'^manage/manager/add/(?P<user_id>\d+)$',views.add_staff_member),
+    url(r'^manage/manager/remove/(?P<user_id>\d+)$',views.remove_staff_member)
 ]
