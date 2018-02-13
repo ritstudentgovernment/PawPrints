78973d14118d814841d407be5ca0e0aef38f13c4
Lukas Yelle <lxy5611@g.rit.edu>
2018-02-13 15:07:23 -0500

    Turns off debug

--- pawprints/settings.py
+++ pawprints/settings.py
@@ -26,7 +26,7 @@ SAML_FOLDER = os.path.join(BASE_DIR, 'saml')
 SECRET_KEY = secrets.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
