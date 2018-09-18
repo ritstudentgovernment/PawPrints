ad5b45f8426439df4b785e9457a9bef8d43c9b70
zujko <peter@zujko.xyz>
2018-09-18 16:36:27 -0400

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
-if [$TRAVIS_BRANCH == "master"]
-then
-  curl -u $USERNAME:$PASSWORD -d '{"author": "'"$AUTHOR_NAME"'", "email": "'"$COMMITTER_EMAIL"'"}' https://pawprints.rit.edu/deploy
-else if [$TRAVIS_BRANCH == "develop"]
-then
-  curl -u $USERNAME:$PASSWORD -d '{"author": "'"$AUTHOR_NAME"'", "email": "'"$COMMITTER_EMAIL"'"}' https://sgstage.rit.edu/deploy
+if [ $TRAVIS_BRANCH = "master" ]; then
+  curl -u $USERNAME:$PASSWORD -d '{"author": "'"$AUTHOR_NAME"'", "email": "'"$COMMITTER_EMAIL"'"}' https://pawprints.rit.edu/deploy ;
+  exit 0;
+elif [ $TRAVIS_BRANCH = "develop" ]; then
+  curl -u $USERNAME:$PASSWORD -d '{"author": "'"$AUTHOR_NAME"'", "email": "'"$COMMITTER_EMAIL"'"}' https://sgstage.rit.edu/deploy ;
 fi
