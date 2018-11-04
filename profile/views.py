b2291e931ea9be5b154c77ebea2a7f299472de98
zujko <peter@zujko.xyz>
2018-11-04 16:48:10 -0500

    Fix authenticate

--- profile/views.py
+++ profile/views.py
@@ -7,7 +7,7 @@ Updated: Feb 16 2018
 import logging
 
 from django.contrib.auth import login as auth_login
-from django.contrib.auth import authenticate, logout
+from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.contrib.auth.models import User
 from django.conf import settings
