5dbe7aa8691437a059bc2eb626c07ff3a5a1faae
Lukas Yelle <lxy5611@g.rit.edu>
2019-02-15 13:31:42 -0500

    Actually fixes everything (that was failing before)!

--- petitions/views.py
+++ petitions/views.py
@@ -709,7 +709,7 @@ def petition_unpublish(request, petition_id):
 def petition_report(request, petition_id):
     user = request.user
     petition = get_object_or_404(Petition, pk=petition_id)
-    previous_report_exists = Report.objects.filter(pk=petition_id, reporter=user).exists()
+    previous_report_exists = Report.objects.filter(petition=petition_id, reporter=user.id).exists()
     if not previous_report_exists:
         reason = request.POST.get('reason')
         report = Report(petition=petition, reporter=user, reported_at=timezone.now(), reported_for=reason)
