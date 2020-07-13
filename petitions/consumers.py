58705a11b23f42223b7cabbc7a96a28a0c01a7e5
Lukas Yelle <lxy5611@g.rit.edu>
2020-07-13 12:46:10 -0500

    Fixes bug where user could not see signed petitions

--- petitions/consumers.py
+++ petitions/consumers.py
@@ -87,8 +87,7 @@ class PetitionConsumer(JsonWebsocketConsumer):
             self.send_json({"command": "get", "petition": json.dumps(petition)})
 
     def send_petitions(self, petitions, command=None):
-        user = self.scope["user"] if hasattr(self.scope, "user") else None
-        petitions = get_petitions_and_map(petitions, user)
+        petitions = get_petitions_and_map(petitions, self.scope["user"])
         if command is not None:
             petitions.update({"command": command})
         self.send_json(petitions)
