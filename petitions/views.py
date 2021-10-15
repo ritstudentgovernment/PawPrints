604e1653f2ba8f308b0e443aecb5895fef5f8c74
Zacharry <zacharryg15@gmail.com>
2021-10-14 21:38:01 -0400

    init news letter section

--- petitions/views.py
+++ petitions/views.py
@@ -73,6 +73,18 @@ def about(request):
     }
     return render(request, 'about.html', data_object)
 
+def news(request):
+    """
+    Handles displaying the news page
+    """
+    data_object = {
+        'name': CONFIG['name'],
+        'main_logo': CONFIG['main_logo'],
+        'generate_top_nav': CONFIG['generate_top_nav'],
+        'analytics_id': settings.ANALYTICS
+    }
+    return render(request, 'news.html', data_object)
+
 
 def maintenance(request):
     return render(request, 'Something_Special.html')
