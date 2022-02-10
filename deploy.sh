17a4e8167630e5e680c4f31e84e99c237c94fe3b
JaTGin <jakeginiewski@gmail.com>
2022-02-09 19:03:20 -0500

    MORE K :))))))

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 if [ $TRAVIS_BRANCH = "master" ]; then
-  curl -u $USERNAME:$PASSWORD -d '{"author": "'"$AUTHOR_NAME"'", "email": "'"$COMMITTER_EMAIL"'"}' https://pawprints.rit.edu/deploy;
+  curl -u $USERNAME:$PASSWORD -d '{"author": "'"$AUTHOR_NAME"'", "email": "'"$COMMITTER_EMAIL"'"}' https://pawprints.rit.edu/deploy -k;
   exit 0;
 elif [ $TRAVIS_BRANCH = "develop" ]; then
-  curl -u $USERNAME:$PASSWORD -d '{"author": "'"$AUTHOR_NAME"'", "email": "'"$COMMITTER_EMAIL"'"}' https://sgstage.rit.edu/deploy;
+  curl -u $USERNAME:$PASSWORD -d '{"author": "'"$AUTHOR_NAME"'", "email": "'"$COMMITTER_EMAIL"'"}' https://sgstage.rit.edu/deploy -k;
 fi
