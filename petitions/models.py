1e8f23f73df846eb0a4d27a9404ec981dc6fa74b
Sma Das <sd4598@g.rit.edu>
2023-03-30 21:40:04 -0400

    Removed required keyword breaking models

--- petitions/models.py
+++ petitions/models.py
@@ -11,8 +11,10 @@ from django.dispatch import receiver
 
 import bleach
 
-bleach.sanitizer.ALLOWED_TAGS.extend([u'i',u'h1', u'h2',u'h3', u'h4', u'h5', u'h6',u'p',u'sub',u'br',u'sup', u'span',u'img'])
-bleach.sanitizer.ALLOWED_ATTRIBUTES[u'img'] = [u'alt', u'height', u'src', u'width']
+bleach.sanitizer.ALLOWED_TAGS.extend(
+    [u'i', u'h1', u'h2', u'h3', u'h4', u'h5', u'h6', u'p', u'sub', u'br', u'sup', u'span', u'img'])
+bleach.sanitizer.ALLOWED_ATTRIBUTES[u'img'] = [
+    u'alt', u'height', u'src', u'width']
 bleach.sanitizer.ALLOWED_STYLES.extend(u'text-align')
 for key in bleach.sanitizer.ALLOWED_ATTRIBUTES.keys():
     bleach.sanitizer.ALLOWED_ATTRIBUTES[key].append(u'data-mce-style')
@@ -34,11 +36,14 @@ class Petition(models.Model):
     expires = models.DateTimeField()
     last_signed = models.DateTimeField(default=None, blank=True, null=True)
     has_response = models.BooleanField(default=False)
-    response = models.ForeignKey('petitions.Response', default=None, blank=True, null=True, on_delete=models.SET_NULL)
+    response = models.ForeignKey(
+        'petitions.Response', default=None, blank=True, null=True, on_delete=models.SET_NULL)
     in_progress = models.NullBooleanField()
-    updates = models.ManyToManyField('petitions.Update', default=None, required=False, null=True, blank=True)
-    old_id = models.CharField(max_length=20, default=None, blank=True, null=True)
-    #committee = models.CharField(max_length=100, default=None)
+    updates = models.ManyToManyField(
+        'petitions.Update', default=None,  null=True, blank=True)
+    old_id = models.CharField(
+        max_length=20, default=None, blank=True, null=True)
+    # committee = models.CharField(max_length=100, default=None)
 
     def __str__(self):
         return self.title
